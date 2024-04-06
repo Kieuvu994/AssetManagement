8904721591f680eeb5b9aed1e0f72e1faff248a3
kieu vu <kieuvu994@gmail.com>
2024-04-06 09:29:57 +0700

    first commit

---
+++ src/test/java/CNPMNC/AssetManagement/AssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package CNPMNC.AssetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
